a4c1c2c3664b10ebc3fdccb6590a95e50665f8d3
Muhammad Shams <mr.shamssheikh@gmail.com>
2020-03-11 01:00:47 +0500

    splash desing completed

--- POS/ViewController.swift
+++ POS/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var dontHaveAnAccount: UIButton!
+    @IBOutlet weak var signIn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        addLeftImageToUITextField(textField: userName , img: #imageLiteral(resourceName: "icons.png") )
+        
+        signIn.layer.cornerRadius = 5
+        signIn.layer.borderColor = UIColor.black.cgColor
+
+        
+        
+        
+    }
+    
+    
+    
+    func addLeftImageToUITextField(textField : UITextField , img : UIImage){
+        
+        let leftImage = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: img.size.width/2, height: img.size.height/2))
+        
+        leftImage.image = img
+        
+        textField.leftView = leftImage
+        textField.leftViewMode = .always
     }
 
 
